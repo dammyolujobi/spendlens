6cf72a53e05853df4fdb144cf0f1a2e8bf199078
dammyjones <daolabmovies@gmail.com>
2026-03-27 16:21:49 +0100

    Update CORS middleware to use dynamic origin from environment variable

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from router import gmail,setup,user
 import uvicorn
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+ORIGIN = os.getenv("ORIGIN")
 app = FastAPI()
 app.include_router(gmail.router)
 app.include_router(setup.router)
 app.include_router(user.router)
 
+
 app.add_middleware(
    CORSMiddleware,
-   allow_origins = ["http://localhost:3000"],
+   allow_origins = [ORIGIN],
    allow_credentials = True,
    allow_methods = ["GET","POST"],
    allow_headers = ["Content-Type","Authorization"]
